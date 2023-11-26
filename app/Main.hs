d5a761d1cf89861318ab89d9cb43682d19f91ef8
Fred Strauss <fred@strider.co.za>
2023-11-26 17:25:52 +0200

    Project creation

---
+++ app/Main.hs
@@ -0,0 +1,35 @@
+module Main where
+
+import qualified Data.Map as M
+import System.Environment (getArgs)
+
+import qualified Day01 (solve)
+import qualified Day02 (solve)
+import qualified Day03 (solve)
+import qualified Day04 (solve)
+import qualified Day05 (solve)
+import qualified Day06 (solve)
+
+solutions :: M.Map String (String -> IO ())
+solutions = M.fromList
+  [ ("01", Day01.solve)
+  , ("02", Day02.solve)
+  , ("03", Day03.solve)
+  , ("04", Day04.solve)
+  , ("05", Day05.solve)
+  , ("06", Day06.solve)
+  ]
+
+solveSingle :: String -> IO ()
+solveSingle day = do
+  putStrLn $ "--- Day " ++ day ++ " ---"
+  case M.lookup day solutions of
+    Just solver -> solver day >> putStrLn ""
+    Nothing     -> putStrLn "Not yet implemented"
+
+main :: IO ()
+main = do
+  args <- getArgs
+  if null args
+    then solveSingle $ fst $ M.findMax solutions
+    else mapM_ solveSingle args
---
+++ src/Lib.hs
@@ -0,0 +1,6 @@
+module Lib
+  ( lib
+  ) where
+
+lib :: IO ()
+lib = putStrLn "Hello Haskell"
