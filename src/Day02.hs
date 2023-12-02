ba2a4417532005ea377071b2695ffe1a3d8edb46
Fred Strauss <fred@strider.co.za>
2023-12-02 07:55:44 +0200

    Day 2 done

--- src/Day02.hs
+++ src/Day02.hs
@@ -3,17 +3,65 @@ module Day02
   ) where
 
 import qualified Text.Parsec as P
+import Text.Parsec ((<|>))
 
 import Helpers
 
-dayParser :: Parser a
-dayParser = undefined
+type Cubes  = (Int, Char)
+type Draw   = [ Cubes ]
+type Game   = (Int, [ Draw ])
+type Games  = [ Game ]
 
-part1 :: a -> IO ()
-part1 input = putStrLn "n/a"
+parseCubes :: Parser Cubes
+parseCubes = do
+  P.spaces
+  amount <- parseInt
+  P.space
+  color <- P.string "red" <|> P.string "green" <|> P.string "blue"
+  return (amount, head color)
 
-part2 :: a -> IO ()
-part2 input = putStrLn "n/a"
+parseDraw :: Parser Draw
+parseDraw = P.sepBy parseCubes (P.char ',')
+
+parseDraws :: Parser [ Draw ]
+parseDraws = P.sepBy parseDraw (P.char ';')
+
+parseGame :: Parser Game
+parseGame = do
+  P.string "Game "
+  game <- parseInt
+  P.char ':'
+  draws <- parseDraws
+  P.newline
+  return (game, draws)
+
+dayParser :: Parser Games
+dayParser = P.many parseGame
+
+isValidCubes :: Cubes -> Bool
+isValidCubes (count, cube)
+  | cube == 'r' = count <= 12
+  | cube == 'g' = count <= 13
+  | cube == 'b' = count <= 14
+  | otherwise   = error $ "Unexpected cube " <> show cube
+
+isValidGame :: Game -> Bool
+isValidGame (game, draws) = all isValidDraw draws
+  where isValidDraw = all isValidCubes
+
+part1 :: Games -> IO ()
+part1 input = print $ sum $ map fst $ filter isValidGame input
+
+minReqCubes :: Game -> (Int, Int, Int)
+minReqCubes (game, draws) = (max 'r', max 'g', max 'b')
+  where getCount cube []          = 0
+        getCount cube ((i,c):ds)  | c == cube = i
+                                  | otherwise = getCount cube ds
+        max cube                  = maximum $ map (getCount cube) draws
+
+part2 :: Games -> IO ()
+part2 input = print $ sum $ map (power . minReqCubes) input
+  where power (r,g,b) = r*g*b
 
 solve :: String -> IO ()
 solve day = do
