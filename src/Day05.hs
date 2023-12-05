7fcdf8d7770424b4dc0b41a7bb72de7f5c00e309
Fred Strauss <fred@strider.co.za>
2023-12-05 21:29:38 +0200

    Day 5 part 1 done

--- src/Day05.hs
+++ src/Day05.hs
@@ -3,17 +3,80 @@ module Day05
   ) where
 
 import qualified Text.Parsec as P
+import Text.Parsec ((<|>))
+import Data.List (transpose, nub)
 
 import Helpers
 
-dayParser :: Parser a
-dayParser = undefined
+data AlmanacMap = AlmanacMap
+  { mapName         :: String
+  , mapSources      :: [Int]
+  , mapDestinations :: [Int]
+  , mapLengths      :: [Int]
+  } deriving (Eq, Show)
 
-part1 :: a -> IO ()
-part1 input = putStrLn "n/a"
+data Almanac = Almanac
+  { seeds :: [Int]
+  , maps  :: [AlmanacMap]
+  } deriving (Eq, Show)
 
-part2 :: a -> IO ()
-part2 input = putStrLn "n/a"
+seeds2 :: Almanac -> [Int]
+seeds2 a = concatMap expand $ pairs $ seeds a
+  where pairs []        = []
+        pairs (a:b:xs)  = (a,b) : pairs xs
+        expand (s,l)    = [s..s+l-1]
+
+newAlmanacMap :: String -> [[Int]] -> AlmanacMap
+newAlmanacMap name vals = AlmanacMap name sources dests lengths
+  where dests   = head $ transpose vals
+        sources = transpose vals !! 1
+        lengths = transpose vals !! 2
+
+parseMapLine :: Parser [Int]
+parseMapLine = do
+  dest <- parseInt
+  P.spaces
+  source <- parseInt
+  P.spaces
+  len <- parseInt
+  P.newline
+  return [dest, source, len]
+
+parseMap :: Parser AlmanacMap
+parseMap = do
+  name <- P.manyTill P.anyChar P.space
+  P.string "map:"
+  P.newline
+  lines <- P.manyTill parseMapLine (P.newline <|> (P.eof >> return '\n'))
+  return $ newAlmanacMap name lines
+
+dayParser :: Parser Almanac
+dayParser = do
+  P.string "seeds:"
+  seeds <- P.manyTill (P.space >> parseInt) P.newline
+  P.newline
+  maps <- P.manyTill parseMap P.eof
+  return $ Almanac seeds maps
+
+convert :: Int -> AlmanacMap -> Int
+convert src am
+  | fits      = d + src - s
+  | otherwise = src
+  where ranges          = zip3 (mapDestinations am) (mapSources am) (mapLengths am)
+        inrange (_,s,l) = src >= s && src < s+l
+        fits            = any inrange ranges
+        (d,s,_)         = head $ filter inrange ranges
+
+convertAll :: Almanac -> Int -> Int
+convertAll a src = foldl convert src $ maps a
+
+part1 :: Almanac -> IO ()
+part1 input = print $ minimum $ map (convertAll input) (seeds input)
+
+part2 :: Almanac -> IO ()
+--part2 input = print $ minimum $ map (convertAll input) (seeds2 input)
+part2 input = do
+  print $ length $ seeds2 input
 
 solve :: String -> IO ()
 solve day = do
