52a5d1dc3787baf22ff8ad4cebbd417e16c902f7
Fred Strauss <fred@strider.co.za>
2023-12-06 07:30:58 +0200

    Day 6 done

--- src/Day06.hs
+++ src/Day06.hs
@@ -2,21 +2,27 @@ module Day06
   ( solve
   ) where
 
-import qualified Text.Parsec as P
+calc :: (Int, Int) -> Int -> Int
+calc (t, wd) hold = hold * (t - hold)
 
-import Helpers
+beats :: (Int, Int) -> Int -> Bool
+beats (t, wd) hold = calc (t, wd) hold > wd
 
-dayParser :: Parser a
-dayParser = undefined
+numbeats :: (Int, Int) -> Int
+numbeats (t, wd) = length $ filter (beats (t, wd)) (tries (t, wd))
+  where tries (t, _)  = [1..t-1]
 
-part1 :: a -> IO ()
-part1 input = putStrLn "n/a"
+part1 :: [(Int, Int)] -> IO ()
+part1 input = print $ product $ map numbeats input
 
-part2 :: a -> IO ()
-part2 input = putStrLn "n/a"
+part2 :: (Int, Int) -> IO ()
+part2 input = print $ numbeats input
 
 solve :: String -> IO ()
 solve day = do
-  parsed <- parseInput day dayParser
-  part1 parsed
-  part2 parsed
+  part1 input
+  part2 input2
+  where input   = [ (54, 302), (94, 1476), (65, 1029), (92, 1404) ]
+        input2  = (54946592, 302147610291404)
+        test    = [ (7, 9), (15, 40), (30, 200) ]
+        test2   = (71530, 940200)
