50ca894e151b2b74a47375e423749a6fa547d2ff
Fred Strauss <fred@strider.co.za>
2023-12-11 21:38:19 +0200

    Day 12 prep

--- app/Main.hs
+++ app/Main.hs
@@ -14,6 +14,7 @@ import qualified Day08 (solve)
 import qualified Day09 (solve)
 import qualified Day10 (solve)
 import qualified Day11 (solve)
+import qualified Day12 (solve)
 
 solutions :: M.Map String (String -> IO ())
 solutions = M.fromList
@@ -28,6 +29,7 @@ solutions = M.fromList
   , ("09", Day09.solve)
   , ("10", Day10.solve)
   , ("11", Day11.solve)
+  , ("12", Day12.solve)
   ]
 
 solveSingle :: String -> IO ()
