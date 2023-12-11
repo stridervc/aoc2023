c123297448280bf1197de0558f8afb1869612945
Fred Strauss <fred@strider.co.za>
2023-12-11 11:06:13 +0200

    Day 11 done

--- src/Day11.hs
+++ src/Day11.hs
@@ -15,22 +15,6 @@ dayParser = do
   return $ G.newGridFromList (Just '.') grid
   where parseLine = P.manyTill P.anyChar P.newline
 
-insertCol :: Int -> Sky -> Sky
-insertCol col sky = emptyCol $ foldl shiftRow sky ys
-  where xs                  = reverse [col..G.maxX sky]
-        ys                  = [G.minY sky..G.maxY sky]
-        shiftTile sky (x,y) = G.putTile (x+1,y) (G.getTile (x,y) sky) sky
-        shiftRow sky y      = foldl (\sky x -> shiftTile sky (x,y)) sky xs
-        emptyCol sky        = foldl (\sky y -> G.putTile (col,y) '.' sky) sky ys
-
-insertRow :: Int -> Sky -> Sky
-insertRow row sky = emptyRow $ foldl shiftCol sky xs
-  where xs                  = [G.minX sky..G.maxX sky]
-        ys                  = reverse [row..G.maxY sky]
-        shiftTile sky (x,y) = G.putTile (x,y+1) (G.getTile (x,y) sky) sky
-        shiftCol sky x      = foldl (\sky y -> shiftTile sky (x,y)) sky ys
-        emptyRow sky        = foldl (\sky x -> G.putTile (x,row) '.' sky) sky xs
-
 isEmptyCol :: Int -> Sky -> Bool
 isEmptyCol col sky = all (=='.') tiles
   where tiles = map (\y -> G.getTile (col,y) sky) [G.minY sky..G.maxY sky]
@@ -39,36 +23,41 @@ isEmptyRow :: Int -> Sky -> Bool
 isEmptyRow row sky = all (=='.') tiles
   where tiles = map (\x -> G.getTile (x,row) sky) [G.minX sky..G.maxX sky]
 
-expandCols :: Sky -> Sky
-expandCols sky = foldr insertCol sky cols
-  where cols  = filter (`isEmptyCol` sky) [G.minX sky..G.maxX sky]
-
-expandRows :: Sky -> Sky
-expandRows sky = foldr insertRow sky rows
-  where rows  = filter (`isEmptyRow` sky) [G.minY sky..G.maxY sky]
-
 galaxies :: Sky -> [G.Coord]
 galaxies sky = filter isGalaxy [(x,y) | x <- [G.minX sky..G.maxX sky], y <- [G.minY sky..G.maxY sky]]
   where isGalaxy pos  = G.getTile pos sky == '#'
 
-expand :: Sky -> Sky
-expand = expandCols . expandRows
-
 manhattan :: G.Coord -> G.Coord -> Int
 manhattan (x1,y1) (x2,y2) = lx - sx + ly - sy
-  where (lx, sx)  | x1 > x2   = (x1,x2)
-                  | otherwise = (x2,x1)
-        (ly, sy)  | y1 > y2   = (y1,y2)
-                  | otherwise = (y2,y1)
+  where (lx, sx)  = (max x1 x2, min x1 x2)
+        (ly, sy)  = (max y1 y2, min y1 y2)
+
+emptyColsBetween :: Sky -> G.Coord -> G.Coord -> Int
+emptyColsBetween sky (x1,_) (x2,_) = length $ filter (`isEmptyCol` sky) [sx..lx]
+  where lx  = max x1 x2
+        sx  = min x1 x2
+
+emptyRowsBetween :: Sky -> G.Coord -> G.Coord -> Int
+emptyRowsBetween sky (_,y1) (_,y2) = length $ filter (`isEmptyRow` sky) [sy..ly]
+  where ly  = max y1 y2
+        sy  = min y1 y2
+
+solveIt :: Int -> Sky -> Int
+solveIt n sky = sum $ map add $ zip3 mtrows mtcols manhat
+  where pairs'      = pairs $ galaxies sky
+        mtrows      = map (uncurry $ emptyRowsBetween sky) pairs'
+        mtcols      = map (uncurry $ emptyColsBetween sky) pairs'
+        manhat      = map (uncurry manhattan) pairs'
+        add (r,c,d) = d + r * (n-1) + c * (n-1)
+
 part1 :: Sky -> IO ()
-part1 sky = print $ sum $ map (uncurry manhattan) $ pairs $ galaxies $ expand sky
+part1 sky = print $ solveIt 2 sky
 
 part2 :: Sky -> IO ()
--- part2 sky = print $ sum $ map (uncurry manhattan) $ pairs $ galaxies $ expand2 1000000 sky
-part2 _ = putStrLn "n/a"
+part2 sky = print $ solveIt 1000000 sky
 
 solve :: String -> IO ()
 solve day = do
-  parsed <- parseTestInput day dayParser
+  parsed <- parseInput day dayParser
   part1 parsed
   part2 parsed
