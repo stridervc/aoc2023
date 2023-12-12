ac32d67ff89e4ac6560b0c68797a0aa01a719135
Fred Strauss <fred@strider.co.za>
2023-12-12 09:34:28 +0200

    Day 12 part 1 done

--- src/Day12.hs
+++ src/Day12.hs
@@ -3,16 +3,73 @@ module Day12
   ) where
 
 import qualified Text.Parsec as P
+import Text.Parsec ((<|>))
+
+import Data.List (group, elemIndex)
+import Data.Maybe (fromJust, isNothing)
 
 import Helpers
 
-dayParser :: Parser a
-dayParser = undefined
+type History  = String
+type Checksum = [Int]
+type Record   = (History, Checksum)
+type Records  = [ Record ]
 
-part1 :: a -> IO ()
-part1 input = putStrLn "n/a"
+parseLine :: Parser Record
+parseLine = do
+  hist <- P.manyTill histchar P.space
+  check <- P.sepBy parseInt (P.char ',')
+  P.newline
+  return (hist, check)
+  where histchar  = P.char '?' <|> P.char '.' <|> P.char '#'
 
-part2 :: a -> IO ()
+dayParser :: Parser Records
+dayParser = P.many parseLine
+
+isValid :: Record -> Bool
+isValid (hist, check) = check == map length (filter (\s -> head s == '#') $ group hist)
+
+-- ...
+-- #..
+-- .#.
+-- ##.
+-- ..#
+-- #.#
+-- .##
+-- ###
+nextHistory :: Record -> History -> Maybe History
+nextHistory (hist,check) curr
+  | '?' `elem` curr   = Just $ map (\c -> if c == '?' then '.' else c) curr
+  | all (=='#') vals  = Nothing
+  | otherwise         = Just $ map (\i -> if hist!!i == '?' then repl i else hist!!i) [0..length hist - 1]
+  where qis         = foldr (\i l -> if hist!!i == '?' then i : l else l) [] [0..length hist - 1]
+        vals        = map (curr!!) qis
+        inc []      = []
+        inc (x:xs)  | x == '.'  = '#' : xs
+                    | x == '#'  = '.' : inc xs
+        nvals       = inc vals
+        repl i      = nvals !! fromJust (elemIndex i qis)
+
+possibilities' :: Record -> History -> [ History ]
+possibilities' (hist, check) curr
+  | isNothing next          = []
+  | isValid (jnext, check)  = jnext : possibilities' (hist, check) jnext
+  | otherwise               = possibilities' (hist, check) jnext
+  where next                = nextHistory (hist, check) curr
+        jnext               = fromJust next
+
+possibilities :: Record -> [ History ]
+possibilities (hist, check) = possibilities' (hist, check) hist
+
+unfold :: Record -> Record
+unfold (hist, check) = (hist', check')
+  where hist'   = concat [ hist, ['?'], hist, ['?'], hist, ['?'], hist, ['?'], hist ]
+        check'  = concat [ check, check, check, check, check ]
+
+part1 :: Records -> IO ()
+part1 input = print $ sum $ map (length . possibilities) input
+
+part2 :: Records -> IO ()
 part2 input = putStrLn "n/a"
 
 solve :: String -> IO ()
